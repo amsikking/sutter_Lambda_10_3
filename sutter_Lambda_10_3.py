eecc7192165271f78d58b1a53f62909e4f35de5e
Alfred Millett-Sikking <amsikking@gmail.com>
2021-12-17 14:22:43 -0800

    Update sutter_Lambda_10_3.py
    Added 'name' argument/attribute to tidy up print statements in a multi-device environment (also allows user to specify 'more friendly' name).

--- sutter_Lambda_10_3.py
+++ sutter_Lambda_10_3.py
@@ -8,20 +8,22 @@ class Controller:
     - Note: to enable serial communicaion use the front of the controller to
     set the default communications port = serial (press: MODE 7 1 4 2)
     '''
-    def __init__(self, which_port, verbose=True):
+    def __init__(self, which_port, name='Lambda 10-3', verbose=True):
+        self.name = name
         self.verbose = verbose
         if self.verbose: print(
-            'Initializing Lambda_10_3 with "Wheel A" = 10 position 25mm...',
+            '%s: opening with "Wheel A" = 10 position 25mm...'%self.name,
             end='')
         try:
             self.port = serial.Serial(port=which_port, timeout=5)
         except serial.serialutil.SerialException:
-            raise IOError('Unable to connect to Lambda_10_3 on %s'%which_port)
+            raise IOError('%s: unable to connect on %s'%(
+                self.name, which_port))
         self.port.write(b'\xFD') # get controller type and configuration
         response = self.port.readline()
         if response != b'\xfd10-3WA-25WB-NCWC-NCSA-VSSB-VS\r':
-            print('Controller response =', response)
-            raise IOError("We don't yet support this Lambda_10_3 configuration")
+            print('%s: response ='%self.name, response)
+            raise IOError("%s: configuration no supported"%self.name)
         if self.verbose: print('done.')
         self._pending_cmd = None
         self.move(0) # use as shutter
@@ -33,14 +35,13 @@ class Controller:
         assert speed in range(8)
         assert wheel == 0
         if self.verbose:
-            print('Moving filter wheel %i to position %i with speed %i... '%(
-                wheel, position, speed), end='')
+            print('%s: moving wheel %i to position %i (speed=%i)'%(
+                self.name, wheel, position, speed))
         cmd = bytes([(wheel << 7) + (speed << 4) + position])
         self.port.write(cmd)
         self._pending_cmd = cmd
         if block:
             self._finish_moving()
-        else: print('\r')
         return None
 
     def _finish_moving(self):
@@ -48,18 +49,18 @@ class Controller:
             return
         response = self.port.read(2)
         if response != self._pending_cmd + b'\r':
-            print('Controller response =', response)
-            raise IOError('Unexpected response from Lambda_10_3')
+            print('%s: response =', response)
+            raise IOError('%s: unexpected response'%self.name)
         assert self.port.in_waiting == 0
         self.position = self._pending_cmd[0] & 0b00001111
         self._pending_cmd = None
-        if self.verbose: print('done.')
+        if self.verbose: print('%s: -> finished moving.'%self.name)
         return None
 
     def close(self):
         self.move(0) # use as shutter
         self.port.close()
-        if self.verbose: print('Closed Lambda_10_3 COM port.')
+        if self.verbose: print('%s: closed.'%self.name)
         return None
 
 if __name__ == '__main__':
